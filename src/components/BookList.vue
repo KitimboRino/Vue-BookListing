c7334c1be444e99a3426a89ed0604bfb257ec412
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-11-21 09:19:29 +0300

    BookList

---
+++ src/components/BookList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookList",
+  data: function() {
+    return{
+    title: "All books"
+    }
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+font-weight: normal;
+}
+</style>
